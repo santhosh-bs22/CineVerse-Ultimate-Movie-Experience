f5f06b38e1435cd72ea842f38b45a2c7aa693b12
santhosh-bs22 <santhosh220506@gmail.com>
2025-09-27 22:14:35 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'samplesearch2',  // 👈 use your repo name here
+  base: 'CineVerse',  // 👈 use your repo name here
 })
